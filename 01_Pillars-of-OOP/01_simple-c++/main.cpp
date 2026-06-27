a49c7574dd2dfa3a84db00358fe6e44082601d16
engrron30 <ronaldtorresrosete30@gmail.com>
2026-06-27 13:48:00 +0800

    Create simple C++

---
+++ 01_Pillars-of-OOP/01_simple-c++/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Tedious to do this
+int main()
+{
+    // Employee
+    string emp1_name = "Ronald";
+    int emp1_age = 26;
+    cout << "Name: " << emp1_name << endl;
+    cout << "Age: " << emp1_age << endl;
+
+    string emp2_name = "Rosete";
+    int emp2_age = 27;
+    cout << "Name: " << emp2_name << endl;
+    cout << "Age: " << emp2_age << endl;
+
+    return 0;
+}
